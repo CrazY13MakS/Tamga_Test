4cd5f566a3ff5777bb2a9dfe34770538d48db885
Maks <crazy13maks@gmail.com>
2018-08-18 22:24:38 +0300

    Добавьте файлы проекта.

---
+++ Tamga_Test_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tamga_Test_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
